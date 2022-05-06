89907bf2297eb7c6357a7392703fd324374e0d8b
simeone22 <47712260+simeone22@users.noreply.github.com>
2022-05-06 19:05:52 +0200

    Messa a posto la navbar, ringraziami

--- navbar.php
+++ navbar.php
@@ -1,51 +1,40 @@
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-</head>
+<?php
+$position = explode(".php", array_slice(explode("/", $_SERVER["REQUEST_URI"]), -1)[0])[0];
+?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="Home.php">Multisala</a>
+        <a class="navbar-brand" href="home.php">Multisala</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav" aria-controls="nav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="nav">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a <?php if($_SESSION['position'] === 'home'){ echo 'class="nav-link active" aria-current="page"';}else echo 'class="nav-link"';?> class="nav-link" aria-current="page" href="home.php"> Home</a>
+                    <a <?php if($position === 'home'){ echo 'class="nav-link active" aria-current="page"';}else echo 'class="nav-link"';?> class="nav-link" aria-current="page" href="home.php"> Home</a>
                 </li>
                 <li class="nav-item">
-                    <a <?php if($_SESSION['position'] === 'prossimamente'){ echo 'class="nav-link active" aria-current="page"';}else echo 'class="nav-link"';?> class="nav-link" aria-current="page" href="prossimamente.php"> Prossimamente</a>
+                    <a <?php if($position === 'prossimamente'){ echo 'class="nav-link active" aria-current="page"';}else echo 'class="nav-link"';?> class="nav-link" aria-current="page" href="prossimamente.php"> Prossimamente</a>
                 </li>
                 <li class="nav-item">
-                    <a <?php if($_SESSION['position'] === 'eventi'){ echo 'class="nav-link active" aria-current="page"';}else echo 'class="nav-link"';?> class="nav-link" aria-current="page" href="eventi.php"> Eventi</a>
+                    <a <?php if($position === 'eventi'){ echo 'class="nav-link active" aria-current="page"';}else echo 'class="nav-link"';?> class="nav-link" aria-current="page" href="eventi.php"> Eventi</a>
                 </li>
             </ul>
             <ul class="navbar-nav">
-                <?php
-                if ($_SESSION["logged"] != null && $_SESSION[false] != null){
-                ?>
+                <?php if (isset($_SESSION["logged"])){?>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <?php
-                        echo $_SESSION["username"];
-                        ?>
+                        <?php echo $_SESSION["username"];?>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                         <li><a class="dropdown-item" href="Area_Personale.jsp"><i class="bi bi-person-fill me-2"></i>Area personale</a></li>
-                        <%if (session.getAttribute("loginType").equals("Cliente")){%>
+                        <?php if ($_SESSION["tipoutente"] == 3) {?>
                         <li>
                             <a class="dropdown-item" href="VisualizzaInterventi.jsp"><i class="bi bi-tools me-2"></i>Interventi</a>
                         </li>
                         <li>
                             <a class="dropdown-item" href="VisualizzaFatture.jsp"><i class="bi bi-receipt me-2"></i>Fatture</a>
                         </li>
-                        <%}else{%>
+                        <?php }elseif($_SESSION["tipoutente"] == 2){?>
                         <li>
                             <a class="dropdown-item" href="GestisciProdotti.jsp"><i class="bi bi-box-seam me-2"></i>Prodotti</a>
                         </li>
@@ -61,43 +50,42 @@
                         <li>
                             <a class="dropdown-item" href="GestisciClienti.jsp"><i class="bi bi-people-fill me-2"></i>Clienti</a>
                         </li>
-                        <?php if ($_SESSION["admin"] == true) {?>
-                        <li>
-                            <a class="dropdown-item" href="GestisciDipendenti.jsp"><i class="bi bi-cone-striped me-2"></i>Dipendenti</a>
-                        </li>
-                        <?php
-                        } ?>
-                        <?php
-                        } ?>
+                        <?php }else{?>
+                            <li>
+                                <a class="dropdown-item" href="VisualizzaInterventi.jsp"><i class="bi bi-tools me-2"></i>Interventi</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="VisualizzaFatture.jsp"><i class="bi bi-receipt me-2"></i>Fatture</a>
+                            </li>
+                        <?php }?>
                         <li><hr class="dropdown-divider"></li>
                         <li><a class="dropdown-item" href="logout.php"><i class="bi bi-box-arrow-right me-2"></i>Logout</a></li>
                     </ul>
                 </li>
-                <%if(session.getAttribute("loginType").equals("Cliente")){%>
+                <?php if ($_SESSION["tipoutente"] == 3) {?>
                 <li class="nav-item mx-3">
                     <a href="Carrello.jsp" class="mt-1 btn btn-outline-primary py-2">
                         <i class="bi bi-cart4"></i>
-                        <%
-                        int PCount = 0;
-                        if(session.getAttribute("carrello") != null){
-                        ArrayList<ProdottiBean> carr = (ArrayList<ProdottiBean>)session.getAttribute("carrello");
-                                for (int i = 0; i < carr.size(); i++) {
-                                PCount += carr.get(i).getNumeroArticoli();
-                                }
-                                }
-                                %>
-                                <span class="badge bg-secondary"><%=PCount%></span>
+                        <?php
+                        $PCount = 0;
+                        if (isset($_SESSION["carrello"]) && $_SESSION["carrello"] > 0) {
+                            for ($i = 0; $i < count($_SESSION["carrello"]); $i++) {
+                                $PCount += $_SESSION["carrello"][$i]["quantita"];
+                            }
+                        }
+                        echo '<span class="badge bg-secondary">' . $PCount . '</span>';
+                        ?>
                     </a>
                 </li>
-                <%}%>
-                <%}else{%>
+                <?php }
+                }else{?>
                 <li class="nav-item">
-                    <a href="Login.jsp" class="nav-link">Login</a>
+                    <a href="login.php" class="nav-link">Login</a>
                 </li>
                 <li class="nav-item">
-                    <a href="Register.jsp" class="nav-link">Registrazione</a>
+                    <a href="registrazione.php" class="nav-link">Registrazione</a>
                 </li>
-                <%}%>
+                <?php }?>
             </ul>
         </div>
         </div>
