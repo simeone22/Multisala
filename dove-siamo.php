e9039f18b9c1829cc206af135ee1762ed15e4f0b
mettiuz <81091794+mettiuz@users.noreply.github.com>
2022-05-11 21:02:18 +0200

    little aggiustamenti

--- dove-siamo.php
+++ dove-siamo.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="Media/fontawesome/css/all.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <link rel="icon" href="Media/Immagini/logo.png">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
@@ -20,7 +20,8 @@
 <?php include "toasts.php";?>
 <?php include "navbar.php" ?>
 <div class="container min-h-85">
-    <i class="fa-solid fa-circle-location-arrow"><h3 class="page-heading mt-5 mb-5"> Dove siamo</h3></i>
+
+    <h3 class="page-heading mt-5 mb-5"><i class="fa-solid fa-circle-location-arrow"></i> Dove siamo</h3>
     <div class="row">
         <div class="col-sm-12 col-lg-12">
             <div class="m18-content-content">
@@ -31,7 +32,7 @@
                     <br>
                     <span style="display: inline; float: none;">28922 Verbania</span>
                 </p>
-                <div id="map" class="w-600 vh-450"></div>
+                <div id="map" class="w-100 vh-50" style="background: white"></div>
                 <script>
                     function sample() {
                         let ll = new google.maps.LatLng(45.93068854564178, 8.571895042620316);
