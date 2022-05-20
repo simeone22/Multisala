09151bb79c2441bd721e2fc5c125751bd11df68c
mettiuz <81091794+mettiuz@users.noreply.github.com>
2022-05-20 22:55:08 +0200

    Terminazione pagina faq

--- faq.php
+++ faq.php
@@ -17,14 +17,14 @@
 <?php include "toasts.php";?>
 <?php include "navbar.php" ?>
 <div class="container">
-    <h5 class="page-heading mt-5 mb-5"><i class="fa-solid fa-comments-question"> Faq </i></h5>
+    <h2 class="page-heading mt-5 mb-5"><i class="fa-solid fa-comments-question"> Faq </i></h2>
     <div class="row">
         <div class="col-sm-12 col-lg-12">
             <div class="m18-content-content">
                 <p>
                     <strong>Posso acquistare dei biglietti per una persona non intestataria del buono?</strong>
                 </p>
-                <p style="font-weight: normal">
+                <p style="font-weight: normal; padding-bottom: 10px;">
                     <font style="font-size: 12pt" size="3">
                         No, i beni e i servizi sono nominali, quindi vendibili solamente ai docenti intestatari del buono.
                     </font>
@@ -32,11 +32,47 @@
                 <p>
                     <strong>Posso prenotare un posto ben preciso?</strong>
                 </p>
-                <p style="font-weight: normal">
+                <p style="font-weight: normal; padding-bottom: 10px;">
                     <font style="font-size: 12pt" size="3">
                         Si, nella pagina prenotazioni è possibile scegliere il numero dei posti e la loro ubicazione.
                     </font>
                 </p>
+                <img src="Media/Immagini/prenotazioni.png" alt="" style="width: 1000px; padding-bottom: 40px;">
+                <p>
+                    <strong>Dove posso vedere le varie posizioni dei multisala?</strong>
+                </p>
+                <p style="font-weight: normal; padding-bottom: 10px;">
+                    <font style="font-size: 12pt" size="3">
+                        Le informazioni geografiche sui vari multisala si trovano nella home.
+                    </font>
+                </p>
+                <p>
+                    <strong>Che informazioni mi fornisce la pagina statistiche?</strong>
+                </p>
+                <p style="font-weight: normal; padding-bottom: 10px;">
+                    <font style="font-size: 12pt" size="3">
+                        La pagina delle statistiche mi permette di controllare il numero di prenotazioni effettuate, grazie anche a filtri
+                        come giorno, mese e anno.
+                    </font>
+                </p>
+                <p>
+                    <strong>Dove ho la possibilità di modificare le mie credenziali?</strong>
+                </p>
+                <p style="font-weight: normal; padding-bottom: 10px;">
+                    <font style="font-size: 12pt" size="3">
+                        Le generalità dell'utente sono ritrovabili nell'area personale in alto a destra, dove possono essere modificate.
+                    </font>
+                </p>
+                <img src="Media/Immagini/screen1.png" alt="" style="width: 1000px; padding-bottom: 40px;">
+                <p>
+                    <strong>Come funzionano le recensioni?</strong>
+                </p>
+                <p style="font-weight: normal; padding-bottom: 10px;">
+                    <font style="font-size: 12pt" size="3">
+                        Ogni utente potrà lasciare delle recensioni munite di voto, da 1 a 5 stelle, ed un commento esplicativo.
+                    </font>
+                </p>
+                <img src="Media/Immagini/recensioni.png" alt="" style="width: 500px; height: 250px ; padding-bottom: 40px;">
             </div>
         </div>
     </div>
