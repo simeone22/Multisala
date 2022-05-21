e0ce680560482c421dafeb321b19fbcc24fa684d
simeone22 <47712260+simeone22@users.noreply.github.com>
2022-05-21 20:50:52 +0200

    Finita pagina statistiche

--- statistiche.php
+++ statistiche.php
@@ -12,7 +12,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.1/dist/chart.min.js"></script>
     <link rel="icon" href="Media/Immagini/logo.png">
-    <title>Statistiche - Multisala</title>
+    <title>Statistiche prenotazioni - Multisala</title>
 </head>
 <body class="d-flex flex-column h-100">
 <?php include "toasts.php";?>
@@ -23,35 +23,60 @@ if(!isset($_SESSION["logged"], $_SESSION["username"], $_SESSION["tipoutente"]) |
 }
 ?>
 <?php include "navbar.php" ?>
-<p class="fs-1 m-3 mb-5"><i class="fa-solid fa-chart-line-up me-3"></i>Statistiche</p>
+<p class="fs-1 m-3 mb-5"><i class="fa-solid fa-chart-line-up me-3"></i>Statistiche prenotazioni</p>
 <div class="form-floating mx-3 mb-3">
     <input type="date" class="form-control" placeholder="Giorno" id="giornoProiezioni" onchange="impostaGiornoProiezioni()" value="<?php
+    $data = date("Y-m-d");
     if(isset($_GET["data"])){
-        echo $_GET["data"];
+        $data = $_GET["data"];
     }
-    else{
-        echo date("Y-m-d");
-    }
+    echo $data;
     ?>" required>
     <label for="giornoProiezioni">Giorno</label>
     <div class="invalid-feedback">Il giorno non può essere vuoto.</div>
 </div>
+<?php
+$connessione = mysqli_connect("localhost", "Lettiero", "Lettiero", "Multisala_Baroni_Lettiero", 12322);
+$query = "SELECT IDFilm, NomeFilm, COUNT(IDPrenotazione) AS NumeroPrenotazioni FROM ((((Film AS F INNER JOIN Proiezioni P on F.IDFilm = P.idFFilm) INNER JOIN Sale AS S ON S.IDSala = P.idFSala) INNER JOIN Cinema AS C ON C.IDCinema = S.idFCinema) INNER JOIN Utenti AS U ON U.IDUtente = C.idFResponsabile AND U.Username = '" . $_SESSION["username"] . "') LEFT JOIN Prenotazioni AS PR ON PR.idFProiezione = P.IDProiezione WHERE ";
+if(!isset($_GET["tipo"]) || $_GET["tipo"] == "day"){
+    $query .= "DATE(OraInizio) = '$data'";
+}elseif($_GET["tipo"] == "week"){
+    $query .= "GETSTARTWEEKDATE(OraInizio) = GETSTARTWEEKDATE('$data')";
+}elseif($_GET["tipo"] == "month"){
+    $query .= "MONTH(OraInizio) = MONTH('$data')";
+}elseif($_GET["tipo"] == "all"){
+    $query = substr($query, 0, -7);
+}
+$query .= " GROUP BY IDFilm";
+$result = mysqli_query($connessione, $query);
+if(!$result){
+    echo "Errore nella query: " . mysqli_error($connessione);
+}else{
+    $ris = $result->fetch_all(MYSQLI_ASSOC);
+    $labels = array();
+    $dati = array();
+    foreach ($ris as $r){
+        $labels[] = $r["NomeFilm"];
+        $dati[] = $r["NumeroPrenotazioni"];
+    }
+}
+?>
 <div class="ms-auto me-3">
-    <button class="btn btn-outline-primary">Giorno</button>
-    <button class="btn btn-outline-primary">Settimana</button>
-    <button class="btn btn-outline-primary">Mese</button>
-    <button class="btn btn-outline-primary">Tutto</button>
+    <button class="btn btn-outline-primary" onclick="impostaGiornoProiezioni('day')">Giorno</button>
+    <button class="btn btn-outline-primary" onclick="impostaGiornoProiezioni('week')">Settimana</button>
+    <button class="btn btn-outline-primary" onclick="impostaGiornoProiezioni('month')">Mese</button>
+    <button class="btn btn-outline-primary" onclick="impostaGiornoProiezioni('all')">Tutto</button>
 </div>
 <canvas id="prenotazioniFilm" style="height: 70vh"></canvas>
 <script>
     const ctx = document.getElementById('prenotazioniFilm').getContext('2d');
     const myChart = new Chart(ctx, {
-        type: 'line',
+        type: 'bar',
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: <?php echo json_encode($labels);?>,
             datasets: [{
                 label: 'Prenotazioni',
-                data: [12, 19, 3, 5, 2, 3],
+                data: <?php echo json_encode($dati);?>,
                 fill: false,
                 borderColor: 'rgb(75, 192, 192)',
                 tension: 0.1
@@ -65,6 +90,12 @@ if(!isset($_SESSION["logged"], $_SESSION["username"], $_SESSION["tipoutente"]) |
             }
         }
     });
+    function impostaGiornoProiezioni(){
+        location.href = "statistiche.php?data=" + document.getElementById('giornoProiezioni').value;
+    }
+    function impostaGiornoProiezioni(tipo){
+        location.href = "statistiche.php?data=" + document.getElementById('giornoProiezioni').value + "&tipo=" + tipo;
+    }
 </script>
 </body>
 </html>
