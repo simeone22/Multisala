02572471e78a2f502ec9c617af486a5cbabb1241
mettiuz <81091794+mettiuz@users.noreply.github.com>
2022-05-21 13:20:36 +0200

    Costruzione pagina film + push immagini

--- film.php
+++ film.php
@@ -16,17 +16,81 @@
 <body class="d-flex flex-column h-100">
 <?php include "toasts.php";?>
 <?php include "navbar.php" ?>
-  <div class="container mt-3">
-      <div class="row movie my-3 align-items-center">
-          <div class="col-12 col-md-4">
-              <div class="movie-images">
-                  <div class="movie-boxes">
-                      <a href="/film/tutankhamon-l-ultima-mostra_2021-40dc0da1-c62d-4a83-a229-4fc53c2508d0">
-                          <img alt="Tutankhamon - L'ultima mostra" src="https://cdn.18tickets.net/impero/uploads/film/playbill/9540/standard_264a533e-a613-422b-9bdd-96114d3a432d.jpg">
-                      </a>
-                      <p style="font-family: 'Arial Black'; text-align: center" class="">
-                          Tutankhamon - L'ultima mostra
-                      </p>
+  <div class="container text-center" style="max-width: 200%; margin-top: 20px;">
+      <div aria-labelledby="parte1" id="parte1">
+          <div class="card card-body" data-parent="parte1" style="position: relative; height: 620px;">
+             <div class="container align-middle" style="padding-top: 50px;">
+                 <div class="row d-flex">
+                     <div class="col-3 d-flex justify-content-center">
+                         <form action="" method="get" style="height: 28%;">
+                             <img src="https://mr.comingsoon.it/imgdb/locandine/big/61728.jpg" alt="..." style="height: 100%;">
+                         </form>
+                     </div>
+                     <div class="col-7 text-start fs-6 mt-3" style="padding-left: 100px;">
+                             <h2 class="visible text-start" style="padding-bottom: 3px;">
+                                 <strong>Tutankhamon - l'ultima mostra</strong>
+                             </h2>
+                             <strong>Regista</strong>
+                             <p>Ernesto Pagano</p>
+                             <strong>Durata</strong>
+                             <p>80 min</p>
+                             <strong>Attori</strong>
+                             <p>Manuel Agnelli, Iggy Pop</p>
+                             <div id="example" class="accordion-item">
+                                 <p id="headingOne" class="accordion-header">
+                                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#dd" aria-expanded="false" aria-controls="dd">
+                                        <strong>Trama</strong>
+                                     </button>
+                                 </p>
+                                 <div id="dd" class="accordion-collapse overflow-auto h-25 collapse" aria-labelledby="headingOne" data-bs-parent="#example">
+                                     <div class="accordion-body">
+                                         <p>
+                                             Tutankhamon. L'ultima mostra, film diretto da Ernesto Pagano, è un documentario si addentra nell'Antico Egitto e, in particolare, nella leggenda di Tutankhamon, una delle figure più misteriose al mondo, nonché il nome legato alla maledizione del faraone. La scoperta della sua tomba nella Valle dei Re risale al 1922 per opera dell'egittologo inglese Howard Carter, che determinato a continuare gli scavi, trovò il primo dei sedici gradini che portavano alla tomba del faraone, una delle più piccole, ma intatta e ricca di corredo di funebre. Carter è stato il primo a guardare all'interno della camera sepolcrale, mediante un piccolo foro eseguito nell'intonaco. Quando gli venne chiesto se riuscisse a scorgere qualcosa, Carter rispose di vedere "cose meravigliose" e, infatti, la camera non solo è rimasta ferma nel tempo, ma aveva al suo interno un grande tesoro: circa 150 oggetti.
+                                             In occasione del centesimo anniversario dal ritrovamento della tomba di Tutankhamon, il documentario ripercorre questa grande scoperta grazie alla mostra internazionale Golden Boy del fotografo Sandro Vanini, che mostra il tesoro della camera funeraria, visibile oggi solo a Il Cairo.
+                                         </p>
+                                     </div>
+                                 </div>
+                             </div>
+                     </div>
+                 </div>
+             </div>
+          </div>
+          <div aria-labelledby="parte2" id="parte2">
+              <div class="card card-body" data-parent="parte2" style="position: relative;">
+                  <div class="container align-middle" style="padding-top: 50px;">
+                      <div class="row d-flex">
+                          <div class="col-3 d-flex justify-content-center">
+                              <form action="" method="get" style="height: 70%;">
+                                  <img src="https://pad.mymovies.it/filmclub/2008/03/090/locandina.jpg" alt="..." style="height: 100%;">
+                              </form>
+                          </div>
+                          <div class="col-7 text-start fs-6 mt-3" style="padding-left: 100px;">
+                                  <h2 class="visible text-start" style="padding-bottom: 3px;">
+                                      <strong>Avatar </strong>
+                                  </h2>
+                                  <strong>Regista</strong>
+                                  <p>James Cameron</p>
+                                  <strong>Durata</strong>
+                                  <p>162 min</p>
+                                  <strong>Attori</strong>
+                                  <p>Zoe Saldana, Sam Worthington, Sigourney Weaver</p>
+                                  <div id="dropdown" class="accordion-item">
+                                      <p id="headingOne" class="accordion-header">
+                                          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#dd2" aria-expanded="false" aria-controls="dd2">
+                                              <strong>Trama</strong>
+                                          </button>
+                                      </p>
+                                      <div id="dd2" class="accordion-collapse overflow-auto h-25 collapse" aria-labelledby="headingOne" data-bs-parent="#dropdown" style="">
+                                          <div class="accordion-body">
+                                              <p>
+                                                  Nel 2154, una compagnia interplanetaria terrestre, la RDA, è da alcuni anni all'opera su Pandora, un mondo primordiale ricoperto da foreste pluviali, le cui flora e fauna sono di dimensioni superiori rispetto a quelle terrestri. Tra le specie del pianeta ve n'è una di umanoidi chiamati Na'vi; per questo sono stati sviluppati gli avatar, cioè ibridi creati in laboratorio con geni umani e geni Na'vi, con ciascun avatar che può essere utilizzato e controllato solo dall'essere umano il cui DNA è stato impiegato per comporlo. Il collegamento viene effettuato grazie a una speciale capsula tecnologica, dove il soggetto trasferisce coscienza e anima nell'avatar.
+                                                  La RDA è interessata ai ricchi giacimenti di unobtainium, un cristallo ferroso che sarebbe la risposta ai gravi problemi energetici della Terra. Un abbondante giacimento è stato individuato proprio sotto l'insediamento del clan Na'vi degli Omaticaya (l'Albero Casa), che la RDA ha tentato di far allontanare attivando il programma avatar, guidato dalla dottoressa Grace Augustine, che è riuscita a insegnare la lingua umana ai Na'vi e ad apprendere la loro cultura, ma i rapporti si sono guastati dopo alcuni incidenti e la via diplomatica viene criticata dal dirigente Parker Selfridge e dal colonnello Miles Quaritch. Intanto approda su Pandora l'ex marine invalido Jake Sully, che ha accettato di rimpiazzare il fratello gemello Tommy, uno scienziato a lungo addestratosi per pilotare un avatar ma che, poco prima di partire, era stato ucciso. Jake, lautamente pagato per sostituire Tommy, accetta l'offerta del colonnello Quaritch: in cambio di informazioni utili per un attacco militare, potrà sottoporsi a una costosa operazione chirurgica sulla Terra e riavere l'uso delle gambe.
+                                              </p>
+                                          </div>
+                                      </div>
+                                  </div>
+                          </div>
+                      </div>
                   </div>
               </div>
           </div>
