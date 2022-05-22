d9c1bdb00cfeac5248aea31ad80cf4123f38bae1
mettiuz <81091794+mettiuz@users.noreply.github.com>
2022-05-22 20:31:29 +0200

    Aggiustamenti pagina film pt 3

--- film.php
+++ film.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="Media/CSS/checkAnimation.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <link rel="icon" href="Media/Immagini/logo.png">
-    <title>Prossimamente - Multisala</title>
+    <title>Film - Multisala</title>
 </head>
 <body class="d-flex flex-column h-100">
 <?php include "toasts.php";?>
@@ -19,25 +19,31 @@
 
 <h2 class="page-heading mt-5 mb-5" style="padding-left: 20px;"><i class="fa-solid fa-films"></i> Film </h2>
   <div class="container text-center" style="max-width: 200%; margin-top: 20px;">
-      <div aria-labelledby="parte1" id="parte1">
-          <div class="card card-body" data-parent="parte1" style="position: relative; height: 620px;">
+      <div aria-labelledby="<?php
+      $sql = "SELECT * FROM ((Proiezioni INNER JOIN Film ON Proiezioni.idFFilm = Film.IDFilm) INNER JOIN AttoriFilm ON Film.IDFilm = AttoriFilm.idFFilm) INNER JOIN Attori ON AttoriFilm.idFAttore = Attori.IDAttore WHERE Privata = 0 GROUP BY IDFilm";
+      $connessione = mysqli_connect("localhost", "Baroni", "Baroni", "Multisala_Baroni_Lettiero", 12322)
+      or die("Errore di connessione al database");
+      $result = $connessione->query($sql);
+      if($result->num_rows > 0){
+      while($row = $result->fetch_assoc()){
+      echo $row["IDFilm"];
+      ?>" id="<?php echo $row["IDFilm"];?>">
+          <div class="card card-body" data-parent="<?php echo $row["IDFilm"];?>" style="position: relative; height: 620px;">
              <div class="container align-middle" style="padding-top: 50px;">
                  <div class="row d-flex">
                      <div class="col-3 d-flex justify-content-center">
-                         <form action="" method="get" style="height: 28%;">
-                             <img src="https://mr.comingsoon.it/imgdb/locandine/big/61728.jpg" alt="..." style="height: 100%;">
+                         <form action="" method="get" style="height: 350px;">
+                             <img src="<?php echo "Media/Film/". $row["IDFilm"]?>.png" alt="..." style="height: 100%;">
                          </form>
                      </div>
                      <div class="col-7 text-start fs-6 mt-3" style="padding-left: 100px;">
                              <h2 class="visible text-start" style="padding-bottom: 3px;">
-                                 <strong>Tutankhamon - l'ultima mostra</strong>
+                                 <strong><?php echo $row["NomeFilm"] ?></strong>
                              </h2>
-                             <strong>Regista</strong>
-                             <p>Ernesto Pagano</p>
                              <strong>Durata</strong>
-                             <p>80 min</p>
+                             <p><?php echo $row["Durata"]?></p>
                              <strong>Attori</strong>
-                             <p>Manuel Agnelli, Iggy Pop</p>
+                             <p><?php echo $row["Nome"] . " " . $row["Cognome"]?></p>
                              <div id="example" class="accordion-item">
                                  <p id="headingOne" class="accordion-header">
                                      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#dd" aria-expanded="false" aria-controls="dd">
@@ -47,7 +53,15 @@
                                  <div id="dd" class="accordion-collapse overflow-auto h-25 collapse" aria-labelledby="headingOne" data-bs-parent="#example">
                                      <div class="accordion-body">
                                          <p>
-                                             Tutankhamon. L'ultima mostra, film diretto da Ernesto Pagano, è un documentario si addentra nell'Antico Egitto e, in particolare, nella leggenda di Tutankhamon, una delle figure più misteriose al mondo, nonché il nome legato alla maledizione del faraone.
+                                             <?php
+                                             if(strlen($row["Trama"]) > 478){
+                                                 $row["Trama"] = substr($row["Trama"], 0, 478) . "...";
+                                                 echo $row["Trama"];
+                                             }
+                                             else {
+                                                 echo $row["Trama"];
+                                             }
+                                             ?>
                                          </p>
                                      </div>
                                  </div>
@@ -56,117 +70,10 @@
                  </div>
              </div>
           </div>
-          <div aria-labelledby="parte2" id="parte2">
-              <div class="card card-body" data-parent="parte2" style="position: relative; margin-top: 30px; height: 620px;">
-                  <div class="container align-middle" style="padding-top: 50px;">
-                      <div class="row d-flex">
-                          <div class="col-3 d-flex justify-content-center">
-                              <form action="" method="get" style="height: 70%;">
-                                  <img src="https://pad.mymovies.it/filmclub/2008/03/090/locandina.jpg" alt="..." style="height: 100%;">
-                              </form>
-                          </div>
-                          <div class="col-7 text-start fs-6 mt-3" style="padding-left: 100px;">
-                                  <h2 class="visible text-start" style="padding-bottom: 3px;">
-                                      <strong>Avatar </strong>
-                                  </h2>
-                                  <strong>Regista</strong>
-                                  <p>James Cameron</p>
-                                  <strong>Durata</strong>
-                                  <p>162 min</p>
-                                  <strong>Attori</strong>
-                                  <p>Zoe Saldana, Sam Worthington, Sigourney Weaver</p>
-                                  <div id="dropdown" class="accordion-item">
-                                      <p id="headingOne" class="accordion-header">
-                                          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#dd2" aria-expanded="false" aria-controls="dd2">
-                                              <strong>Trama</strong>
-                                          </button>
-                                      </p>
-                                      <div id="dd2" class="accordion-collapse overflow-auto h-25 collapse" aria-labelledby="headingOne" data-bs-parent="#dropdown" style="">
-                                          <div class="accordion-body">
-                                              <p>
-                                                  Nel 2154, una compagnia interplanetaria terrestre, la RDA, è da alcuni anni all'opera su Pandora, un mondo primordiale ricoperto da foreste pluviali, le cui flora e fauna sono di dimensioni superiori rispetto a quelle terrestri. Tra le specie del pianeta ve n'è una di umanoidi chiamati Na'vi; per questo sono stati sviluppati gli avatar, cioè ibridi creati in laboratorio con geni umani e geni Na'vi, con ciascun avatar che può essere utilizzato e controllato solo dall'essere umano il cui DNA è stato impiegato per comporlo. Il collegamento viene effettuato grazie a una speciale capsula tecnologica, dove il soggetto trasferisce coscienza e anima nell'avatar.
-                                                  La RDA è interessata ai ricchi giacimenti di unobtainium, un cristallo ferroso che sarebbe la risposta ai gravi problemi energetici della Terra.
-                                              </p>
-                                          </div>
-                                      </div>
-                                  </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              <div id="parte3" aria-labelledby="parte3">
-                  <div class="card card-body" data-parent="parte3" style="position: relative; margin-top: 30px; height: 620px;">
-                      <div class="container align-middle" style="padding-top: 50px;">
-                          <div class="row d-flex">
-                              <div class="col-3 d-flex justify-content-center">
-                                  <form action="" style="height: 28%;">
-                                      <img src="https://mr.comingsoon.it/imgdb/locandine/big/58183.jpg" alt="" style="height: 100%;">
-                                  </form>
-                              </div>
-                              <div class="col-7 text-start fs-6 mt-3" style="padding-left: 100px;">
-                                  <h2 class="visible text-start" style="padding-bottom: 3px;">Shang-Chi: la leggenda dei 10 anelli</h2>
-                                  <strong>Regista</strong>
-                                  <p>Destin Daniel Cretton</p>
-                                  <strong>Durata</strong>
-                                  <p>132 min</p>
-                                  <strong>Attori</strong>
-                                  <p>Simu Liu, Fala Chen, Benedict Wong.</p>
-                                  <div id="example" class="accordion-item">
-                                      <p id="headingOne" class="accordion-header">
-                                          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#dd3" aria-expanded="false" aria-controls="dd3">
-                                            <strong>Trama</strong>
-                                          </button>
-                                      </p>
-                                      <div id="dd3" class="accordion-collapse collapse overflow-auto h-25" aria-labelledby="headingOne" data-bs-parent="#example">
-                                          <div class="accordion-body">
-                                              <p>Per mille anni, Wenwu, oggi conosciuto come il Mandarino, ha sfruttato i poteri dei Dieci Anelli per creare l'omonima organizzazione terroristica e manipolare gli eventi a proprio vantaggio. Nel 1996, in cerca del leggendario villaggio di Ta-Lo, in Cina, ebbe modo di scontrarsi con la sua futura moglie, Jiang Li. Seppur perdendo, l'uomo si infatuò della donna, amore per il quale venne ricambiato e abbandonò il suo precedente stile di vita. Qualche anno dopo, Jiang Li venne uccisa da una gang criminale nemica di Wenwu e quest'ultimo si vendicò uccidendoli tutti, seppur non trovandone il capo, e tornò alla sua precedente vita, includendo anche l'allora figlio di soli 7 anni, Shang-Chi. Addestrato a essere un assassino, a 14 anni Shang-Chi uccise, su ordine di Wenwu, il capo della gang che uccise la madre, ma dopo ciò decise di fuggire da casa, lasciando indietro la sorella Xialing. </p>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-                  <div id="parte4" aria-labelledby="parte4">
-                      <div class="card card-body" data-parent="parte4" style="position: relative; margin-top: 30px; height: 620px;">
-                          <div class="container align-middle" style="padding-top: 50px;">
-                              <div class="row d-flex">
-                                  <div class="col-3 d-flex justify-content-center">
-                                      <form action="" style="height: 55%;">
-                                          <img src="https://lumiere-a.akamaihd.net/v1/images/p_avengersendgame_19751_e14a0104.jpeg?region=0%2C0%2C540%2C810" alt="" style="height: 100%;">
-                                      </form>
-                                  </div>
-                                  <div class="col-7 text-start fs-6 mt-3" style="padding-left: 100px;">
-                                      <h2 class="visible text-start" style="padding-bottom: 3px;">Avengers: endgame</h2>
-                                      <strong>Regista</strong>
-                                      <p>Anthony e Joe Russo</p>
-                                      <strong>Durata</strong>
-                                      <p>181 min</p>
-                                      <strong>Attori</strong>
-                                      <p>Robert downey jr, Chris Evans, Scarlett johansson...</p>
-                                      <div id="example" class="accordion-item">
-                                          <p id="headingOne" class="accordion-header">
-                                              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#dd4" aria-expanded="false" aria-controls="dd4">
-                                                  <strong>Trama</strong>
-                                              </button>
-                                          </p>
-                                          <div id="dd4" class="accordion-collapse collapse overflow-auto h-25" aria-labelledby="headingOne" data-bs-parent="#example">
-                                              <div class="accordion-body">
-                                                  <p>In seguito allo schiocco di dita di Thanos, Clint Barton vede scomparire tutta la sua famiglia. Ventidue giorni più tardi, Tony Stark e Nebula, che si trovano sull'astronave dei Guardiani della Galassia, vengono salvati e portati al quartier generale degli Avengers da Captain Marvel. I tre si ricongiungono con Pepper Potts, Steve Rogers, Natasha Romanoff, Bruce Banner, James Rhodes, Thor e Rocket. Il gruppo, speranzoso di utilizzare le Gemme per riportare tutti in vita, localizza Thanos, ma, una volta raggiunto, quest'ultimo spiega di averle distrutte sfruttando il loro stesso potere, cosa che gli è quasi costata la vita. Appreso ciò, Thor, furioso, lo decapita... </p>
-                                              </div>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              <div class="container">
-                  <hr class="featurette-divider">
-              </div>
-          </div>
       </div>
+      <?php
+      }
+      }?>
   </div>
 </body>
 </html>
