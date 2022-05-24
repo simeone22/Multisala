ec5e9ce906da5d87f9d7aa3132b54e156465efea
mettiuz <81091794+mettiuz@users.noreply.github.com>
2022-05-24 20:35:24 +0200

    last push

--- home.php
+++ home.php
@@ -25,7 +25,51 @@
 <body class="d-flex flex-column h-100">
 <?php include "toasts.php"; ?>
 <?php include "navbar.php" ?>
+    <h2 class="page-heading mt-5 mb-5" style="margin-left: 70px;"><i class="fa-solid fa-house-chimney"></i> Home </h2>
     <div style="height:130%" class="container">
+        <div id="carouselVideoExample" class="carousel slide carousel-fade" data-mdb-ride="carousel">
+            <!-- Indicators -->
+            <div class="carousel-indicators">
+                <button type="button" data-mdb-target="#carouselVideoExample" data-mdb-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-mdb-target="#carouselVideoExample" data-mdb-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-mdb-target="#carouselVideoExample" data-mdb-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+
+            <!-- Inner -->
+            <div class="carousel-inner">
+                <!-- Single item -->
+                <div class="carousel-item active" data-mdb-interval="30000" >
+                    <video class="img-fluid" autoplay loop muted >
+                        <source src="Media/Immagini/1.mp4" type="video/mp4"/>
+                    </video>
+                </div>
+
+                <!-- Single item -->
+                <div class="carousel-item" data-mdb-interval="31000">
+                    <video class="img-fluid" autoplay loop muted>
+                        <source src="Media/Immagini/2.mp4" type="video/mp4"/>
+                    </video>
+                </div>
+
+                <!-- Single item -->
+                <div class="carousel-item" data-mdb-interval="45000">
+                    <video class="img-fluid" autoplay loop muted>
+                        <source src="Media/Immagini/3.mp4" type="video/mp4"/>
+                    </video>
+                </div>
+            </div>
+            <!-- Inner -->
+
+            <!-- Controls -->
+            <button class="carousel-control-prev" type="button" data-mdb-target="#carouselVideoExample" data-mdb-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-mdb-target="#carouselVideoExample" data-mdb-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
         <div class="container text-center p-5">
             <h1 class="display-4 fw-bold">Sedi multisala</h1>
         </div>
@@ -80,5 +124,8 @@
             <p class="text-center text-muted">© 2022 Multisala, tech</p>
         </footer>
     </div>
+
+<script src="Media/JS/mdb.min.js"></script>
 </body>
 </html>
+
