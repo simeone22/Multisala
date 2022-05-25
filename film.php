b8765b9cf27cb57f8ec9dcd58acceac23df1d2c1
simeone22 <47712260+simeone22@users.noreply.github.com>
2022-05-25 02:39:19 +0200

    Fatto filtro per ricerca film. Progetto finito!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- film.php
+++ film.php
@@ -18,9 +18,68 @@
 <?php include "navbar.php" ?>
 
 <h2 class="page-heading mt-5 mb-5" style="padding-left: 20px;"><i class="fa-solid fa-films"></i> Film </h2>
+<div class="m-auto input-group w-75 justify-content-center mb-5 mt-2">
+    <div class="form-floating w-25">
+        <select id="RAttore" class="form-control">
+            <option value="%" <?php if(!isset($_GET["attore"])) echo "selected"?>>Tutti</option>
+            <?php
+            $query = "SELECT * FROM Attori";
+            $connessione = mysqli_connect("localhost", "Lettiero", "Lettiero", "Multisala_Baroni_Lettiero", 12322);
+            $result = $connessione->query($query);
+            while ($row = $result->fetch_assoc()) {
+                echo "<option value='" . $row['IDAttore'] . "'";
+                if(isset($_GET["attore"]) && $_GET["attore"] == $row['IDAttore']) echo "selected";
+                echo ">" . $row['Nome'] . " " . $row['Cognome'] . "</option>";
+            }
+            ?>
+        </select>
+        <label class="form-label" for="RAttore">Attore</label>
+    </div>
+    <div class="form-floating w-25">
+        <select id="RCategoria" class="form-control">
+            <option value="%" <?php if(!isset($_GET["categoria"])) echo "selected"?>>Tutti</option>
+            <?php
+            $query = "SELECT * FROM Categorie";
+            $connessione = mysqli_connect("localhost", "Lettiero", "Lettiero", "Multisala_Baroni_Lettiero", 12322);
+            $result = $connessione->query($query);
+            while ($row = $result->fetch_assoc()) {
+                echo "<option value='" . $row['IDCategoria'] . "'";
+                if(isset($_GET["categoria"]) && $_GET["categoria"] == $row['IDCategoria']) echo "selected";
+                echo ">" . $row['NomeCategoria'] . "</option>";
+            }
+            ?>
+        </select>
+        <label class="form-label" for="RCategoria">Categoria</label>
+    </div>
+    <div class="form-floating" style="width: 10%">
+        <input type="number" min="1" id="durata" class="form-control" placeholder="Durata" <?php if(isset($_GET["durata"])) echo "value='" . $_GET["durata"] . "'"?>>
+        <label class="form-label" for="durata">Durata</label>
+    </div>
+    <div class="form-floating w-25">
+        <input autocomplete="off" type="search" id="ricerca" class="form-control" placeholder="Nome del film" <?php if(isset($_GET["nome"])) echo "value='" . $_GET["nome"] . "'"?>>
+        <label class="form-label" for="ricerca">Nome del film</label>
+    </div>
+    <button type="button" class="btn btn-dark" id="searchButton" onclick="cercaFilm()">
+        <i class="fa-solid fa-search"></i>
+    </button>
+</div>
   <div class="container text-center" style="max-width: 200%; margin-top: 20px;">
       <?php
-      $sql = "SELECT * FROM ((Film INNER JOIN AttoriFilm ON Film.IDFilm = AttoriFilm.idFFilm) INNER JOIN Attori ON AttoriFilm.idFAttore = Attori.IDAttore) LEFT OUTER JOIN Proiezioni ON Proiezioni.idFFilm = Film.IDFilm WHERE Privata = 0  OR Privata IS NULL GROUP BY IDFilm";
+      $sql = "SELECT * FROM (((Film INNER JOIN AttoriFilm ON Film.IDFilm = AttoriFilm.idFFilm) INNER JOIN Attori ON AttoriFilm.idFAttore = Attori.IDAttore) INNER JOIN CategorieFilm CF ON CF.idFFilm = Film.IDFilm) LEFT OUTER JOIN Proiezioni ON Proiezioni.idFFilm = Film.IDFilm WHERE (Privata = 0  OR Privata IS NULL)";
+
+      if(isset($_GET["nome"])){
+          $sql .= " AND NomeFilm LIKE '%".$_GET["nome"]."%'";
+      }
+      if(isset($_GET["attore"])){
+          $sql .= " AND Attori.IDAttore = ".$_GET["attore"];
+      }
+      if(isset($_GET["categoria"])){
+          $sql .= " AND CF.idFCategoria = ".$_GET["categoria"];
+      }
+      if(isset($_GET["durata"])){
+          $sql .= " AND Durata = ".$_GET["durata"];
+      }
+      $sql .= " GROUP BY IDFilm";
       $connessione = mysqli_connect("localhost", "Baroni", "Baroni", "Multisala_Baroni_Lettiero", 12322)
       or die("Errore di connessione al database");
       $result = $connessione->query($sql);
@@ -85,5 +144,28 @@
       }
       }?>
   </div>
+<script>
+    function cercaFilm(){
+        var nome = document.getElementById("ricerca").value;
+        var categoria = document.getElementById("RCategoria").value;
+        var attore = document.getElementById("RAttore").value;
+        var durata = document.getElementById("durata").value;
+        var url = "film.php?";
+        if(nome != ""){
+            url += "nome=" + nome + "&";
+        }
+        if(categoria != "%"){
+            url += "categoria=" + categoria + "&";
+        }
+        if(attore != "%"){
+            url += "attore=" + attore + "&";
+        }
+        if(durata != ""){
+            url += "durata=" + durata + "&";
+        }
+        url = url.substring(0, url.length - 1);
+        window.location.href = url;
+    }
+</script>
 </body>
 </html>
